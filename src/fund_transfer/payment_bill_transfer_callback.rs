70a4c8c5ed3e63e9fc1cd192a3a9b5816450d383
Parama Danoesubroto <paramaw@gmail.com>
2024-04-27 23:10:46 -0400

    Added fund transfer callback

---
+++ src/fund_transfer/payment_bill_transfer_callback.rs
@@ -0,0 +1,37 @@
+use chrono::NaiveDateTime;
+use serde::{Serialize, Deserialize};
+use std::vec::Vec;
+
+use super::FeeDeductType;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PaymentBill {
+    pub merchant_id: Option<String>,
+    pub payment_bill_num: Option<String>,
+    pub out_payment_bill_num: Option<String>,
+    pub payment_status: Option<String>,
+    pub payment_amount: Option<i64>,
+    pub payment_fee: Option<i64>,
+    pub balance_amount: Option<i64>,
+    pub payment_config_fee: Option<i64>,
+    pub fee_deduct_type: Option<FeeDeductType>,
+    pub finish_time: Option<NaiveDateTime>,
+    pub list: Option<Vec<PaymentBillSub>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PaymentBillSub {
+    pub payment_bill_num: Option<String>,
+    pub payment_sub_num: Option<String>,
+    pub out_payment_sub_num: Option<String>,
+    pub receiving_name: Option<String>,
+    pub receiving_bank_id: Option<String>,
+    pub receiving_bank_name: Option<String>,
+    pub receiving_account: Option<String>,
+    pub credited_amount: Option<i64>,
+    pub balance_amount: Option<i64>,
+    pub payment_fee: Option<i64>,
+    pub payment_status: Option<String>,
+    pub error_msg: Option<String>,
+    pub receipt: Option<String>,
+}
